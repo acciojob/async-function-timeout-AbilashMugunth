a510abf1e80a5ecd3c85721a196148fba2073260
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-29 20:44:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+let text = document.querySelector("#text");
+let delay = document.querySelector("#delay");
+let btn = document.querySelector("#btn");
+let output = document.querySelector("#output");
+
+btn.addEventListener("click", () => {
+  hello(text.value, delay.value);
+});
+
+async function hello(text, delay) {
+  const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(text);
+    }, delay);
+  });
+
+  const data = await p1;
+  output.innerHTML = data;
+}
